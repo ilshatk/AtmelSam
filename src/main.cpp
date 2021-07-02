9eb59409eed92786f78955d18ad30b064d4796a2
ilshatk <khisamov98@inbox.ru>
2021-07-02 17:59:40 +0500

    С

--- src/main.cpp
+++ src/main.cpp
@@ -93,41 +93,43 @@ void setup()
 
   //----setup for A32---------------------------------------------------------------------------------------
   
+  //----setup for A33---------------------------------------------------------------------------------------
+
+  ArtSensor AfterSiatB14(1, ("B14"),1, ArtSensor::SENSOR_TYPE_BASIC, 0, 0,false); // Фотодатчик после Сиата 2 (B14) (тип R)
+  ArtSensor M7Perepoln(2, ("B15"), 1, ArtSensor::SENSOR_TYPE_BASIC, 0, 0,false); // Фотодатчик переполнения М7 (B15) (тип R)
+  ArtSensor M7End(3, ("B7"), 2, ArtSensor::SENSOR_TYPE_BASIC, 0, 0,false); // Датчик пушер 1 дома (B7) (тип геркон)
+  ArtSensor M8(4, ("B8"), 3, ArtSensor::SENSOR_TYPE_BASIC, 0, 0,false); // Датчик пушер 1 НЕ дома (B8) (тип геркон)
+  ArtSensor M5Perepoln(5, ("B9"), 4, ArtSensor::SENSOR_TYPE_BASIC, 0, 0,false); // Фотодатчик переполнения М5 (B9) (тип R)
+  ArtSensor M5End(6, ("B10"), 5, ArtSensor::SENSOR_TYPE_BASIC, 0, 0,false); // Фотодатчик на конце М5 (B10) (тип R)
+  ArtSensor M6Start(7, ("B11"), 6, ArtSensor::SENSOR_TYPE_BASIC, 0, 0,false); // Фотодатчик на начале М6 пикпойнт (B11) (тип BGS)
+  ArtSensor M6Start2(8, ("B12"), 7, ArtSensor::SENSOR_TYPE_BASIC, 0, 0,false); // Фотодатчик на начале M6 пикпойнт (B12) (тип BGS)
+  ArtSensor M6End(9, ("B13"), 8, ArtSensor::SENSOR_TYPE_BASIC, 0, 0,false); // Фотодатчик на конце М6 пикпойнт (B13) (тип BGS)
+
+  ArtCylinder Clamp1(12, ("SV1"), 10, 10, false, true, ArtCylinder::MONO_STABLE,
+                     7, 7, &PushNoHome, &PushHome); //Пушер SV1
+
+  ArtDriver M4DRV(10, ("M4DRV"), ArtDriver::DRIVER_TYPE_1, 9, 1, 1, true, 0, 2, 0, 2, 0, 0, 0);//Драйвер на M4
+  ArtDriver M5DRV(11, ("M4DRV"), ArtDriver::DRIVER_TYPE_1, 10, 2, 2, true, 0, 2, 0, 2, 0, 0, 0);//Драйвер на M5
+  ArtDriver M6DRV(12, ("M4DRV"), ArtDriver::DRIVER_TYPE_1, 11, 3, 3, true, 0, 2, 0, 2, 0, 0, 0);//Драйвер на M6
+
+  ArtConveyor2Type ConvM6(13, ("Conveyor3"), ArtConveyor2Type::CONVEYOR_TYPE_2, &M6DRV, &B6, &M5End, 4000, 4, 9000);
+  ArtConveyor1Type ConvM5(14, ("ConvM4"), ArtConveyor1Type::CONVEYOR_TYPE_1, &M5DRV, &B5, &B6, &ConvM6, 1000, 0);
+  ArtConveyor1Type ConvM4(15, ("ConvM4"), ArtConveyor1Type::CONVEYOR_TYPE_1, &M4DRV, &B5, &B6, &ConvM5, 1000,0);
+  //----setup for A33---------------------------------------------------------------------------------------
   
   
-  
-  
-  //ArtConveyor1AType Conv2(9, ("Conveyor2"), ArtConveyor1Type::CONVEYOR_TYPE_1, &Act2, &Sens1, &Sens2, &Conv3, 1000, 0,3);
-
-
-
-  /*
-  ArtSensor Sens1(1, ("Sensor1"), 1, ArtSensor::SENSOR_TYPE_BASIC, 0, 0); // Пусть этот будет входным датчиком;
-  ArtSensor Sens2(2, ("Sensor2"), 2, ArtSensor::SENSOR_TYPE_BASIC, 0, 0); // Пусть этот будет выходным датчиком
-  ArtSensor Sens3(3, ("Sensor3"), 3, ArtSensor::SENSOR_TYPE_BASIC, 0, 0); // Пусть этот будет входным датчиком2;
-  ArtSensor Sens4(4, ("Sensor4"), 4, ArtSensor::SENSOR_TYPE_BASIC, 0, 0); // Пусть этот будет выходным датчиком3;
-  ArtDriver Act1(1, ("Actuator1"), ArtDriver::DRIVER_TYPE_1, 9, true, 0, 2, 0, 2, 0, 0, 0);
-  ArtDriver Act2(2, ("Actuator2"), ArtDriver::DRIVER_TYPE_1, 10, true, 0, 2, 0, 2, 0, 0, 0);
-  ArtDriver Act3(3, ("Actuator3"), ArtDriver::DRIVER_TYPE_1, 11, true, 0, 2, 0, 2, 0, 0, 0);
-  ArtConveyor2Type Conv3(8, ("Conveyor3"), ArtConveyor2Type::CONVEYOR_TYPE_2, &Act3, &Sens3, &Sens4, 4000, 4, 9000);
-  ArtConveyor1AType Conv2(9, ("Conveyor2"), ArtConveyor1Type::CONVEYOR_TYPE_1, &Act2, &Sens1, &Sens2, &Conv3, 1000, 0,3);
-*/
-
-  //ArtConveyor1EType Conv1(10, ("Conveyor1"), ArtConveyor1Type::CONVEYOR_TYPE_1E, &Act1, &Sens1, &Sens2, &Conv2, 6000, 0);
-  // ArtCylinder::ArtCylinder(int id, const char name[], int CloseTime, int OpenTime , bool TimeoutControl , bool CylinderSet, distType type, int cylOpenOut, int cylCloseOut)
-  //Sensors---ArtSensor(int id, const char name[], int SensorInput, SensorType type, int delayRe, int delayFe) ---------------------------------------------------------------------
-  /*ArtSensor PallONConvey(1, ("PallONConvey"), 1, ArtSensor::SENSOR_TYPE_BASIC, 0, 0);
-  ArtSensor PalletsInStack(2, ("PalletsInStack"), 2, ArtSensor::SENSOR_TYPE_BASIC, 0, 0);
-  ArtSensor Clamp1Open(3, ("Clamp1Open"), 3, ArtSensor::SENSOR_TYPE_BASIC, 0, 0);
-  ArtSensor Clamp1Close(4, ("Clamp1Close"), 4, ArtSensor::SENSOR_TYPE_BASIC, 0, 0);
-  ArtSensor Clamp2Open(5, ("Clamp2Open"), 5, ArtSensor::SENSOR_TYPE_BASIC, 0, 0);
-  ArtSensor Clamp2Close(6, ("Clamp2Close"), 6, ArtSensor::SENSOR_TYPE_BASIC, 0, 0);
-  ArtSensor TOPCylinderOpen(7, ("TOPCylinderOpen"), 7, ArtSensor::SENSOR_TYPE_BASIC, 0, 0);
-  ArtSensor TOPCylinderClose(8, ("TOPCylinderClose"), 8, ArtSensor::SENSOR_TYPE_BASIC, 0, 0);
-  ArtSensor BOTCylinderOpen(9, ("BOTCylinderOpen"), 9, ArtSensor::SENSOR_TYPE_BASIC, 0, 0);
-  ArtSensor BOTCylinderClose(10, ("BOTCylinderClose"), 10, ArtSensor::SENSOR_TYPE_BASIC, 0, 0);
-  //Cylinders  ArtCylinder(int id, const char name[], int CloseTime, int OpenTime, bool TimeoutControl, bool CylinderSet,
-  //distType type, int cylOpenOut, int cylCloseOut,ArtSensor *cylOpenIn,ArtSensor *cylCloseIn);
+ 
+ /*/----setup for Dispenser---------------------------------------------------------------------------------------
+  ArtSensor PallONConvey(1, ("PallONConvey"), 1, ArtSensor::SENSOR_TYPE_BASIC, 0, 0,false);
+  ArtSensor PalletsInStack(2, ("PalletsInStack"), 2, ArtSensor::SENSOR_TYPE_BASIC, 0, 0,false);
+  ArtSensor Clamp1Open(3, ("Clamp1Open"), 3, ArtSensor::SENSOR_TYPE_BASIC, 0, 0,false);
+  ArtSensor Clamp1Close(4, ("Clamp1Close"), 4, ArtSensor::SENSOR_TYPE_BASIC, 0, 0,false);
+  ArtSensor Clamp2Open(5, ("Clamp2Open"), 5, ArtSensor::SENSOR_TYPE_BASIC, 0, 0,false);
+  ArtSensor Clamp2Close(6, ("Clamp2Close"), 6, ArtSensor::SENSOR_TYPE_BASIC, 0, 0,false);
+  ArtSensor TOPCylinderOpen(7, ("TOPCylinderOpen"), 7, ArtSensor::SENSOR_TYPE_BASIC, 0, 0,false);
+  ArtSensor TOPCylinderClose(8, ("TOPCylinderClose"), 8, ArtSensor::SENSOR_TYPE_BASIC, 0, 0,false);
+  ArtSensor BOTCylinderOpen(9, ("BOTCylinderOpen"), 9, ArtSensor::SENSOR_TYPE_BASIC, 0, 0,false);
+  ArtSensor BOTCylinderClose(10, ("BOTCylinderClose"), 10, ArtSensor::SENSOR_TYPE_BASIC, 0, 0,false);
 
   ArtCylinder Clamp1(11, ("Clamp1"), 10, 10, false, true, ArtCylinder::MONO_STABLE,
                      1, 2, &Clamp1Open, &Clamp1Close);
@@ -144,8 +146,8 @@ void setup()
                           &BOTCylinderClose);
 
   PalletMagazine Dispenser(15, ("Dispenser"), &Clamp1, &Clamp2, &TOPCylinder,
-                           &BOTCylinder, 9, &PallONConvey, &PalletsInStack, 10, 11);*/
-
+                           &BOTCylinder, 9, &PallONConvey, &PalletsInStack, 10, 11);
+//----setup for Dispenser---------------------------------------------------------------------------------------*/
   while (1)
   {
     if (EASYCAT.MainTask() == ESM_OP) // execute the EasyCAT task
